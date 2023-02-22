0d7f64cff392a4d26b569a76bbcadadc772693f2
Sterre Siekman <sterresiekman@gmail.com>
2023-02-22 21:02:41 +0100

    Change layout of entire app

--- ui.R
+++ ui.R
@@ -2,6 +2,7 @@
 library(shiny)
 library(bs4Dash)
 
+source("utils.R")
 source("mod_airport_busyness.R")
 source("mod_flights_from_NY.R")
 source("mod_view_flight_data.R")
@@ -12,17 +13,43 @@ shiny::shinyUI(
         header = bs4Dash::dashboardHeader(
             title = "Take flight"
         ),
-        sidebar = bs4Dash::dashboardSidebar(),
+        sidebar = bs4Dash::dashboardSidebar(
+            bs4Dash::sidebarMenu(
+                bs4Dash::menuItem(
+                    "Flight departures from New York",
+                    tabName = "flights_info"
+                ),
+                bs4Dash::menuItem(
+                    "Analysis of delays",
+                    tabName = "delays"
+                ),
+                bs4Dash::menuItem(
+                    "View the flights data",
+                    tabName = "view_data"
+                )
+            )
+        ),
         body = bs4Dash::dashboardBody(
-            fluidRow(
-                airport_busyness_ui(id = "airport_busyness_1"),
-                flights_from_NY_ui(id = "flights_from_NY_1")
-            ),
-            fluidRow(
-              model_ui(id = "model_1")  
-            ),
-            fluidRow(
-                view_flight_data_ui(id = "view_flight_data_1")
+            bs4Dash::tabItems(
+                bs4Dash::tabItem(
+                    tabName = "flights_info",
+                    fluidRow(
+                        airport_busyness_ui(id = "airport_busyness_1"),
+                        flights_from_NY_ui(id = "flights_from_NY_1")
+                    )
+                ),
+                bs4Dash::tabItem(
+                    tabName = "delays",
+                    fluidRow(
+                        model_ui(id = "model_1")  
+                    )
+                ),
+                bs4Dash::tabItem(
+                    tabName = "view_data",
+                    fluidRow(
+                        view_flight_data_ui(id = "view_flight_data_1")
+                    )
+                )
             )
         ),
         controlbar = bs4Dash::dashboardControlbar(),
